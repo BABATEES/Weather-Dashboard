b027f10e9e4c5879089ef86168f135c7692e46d5
oluwatayo Hassan <tayohas@gmail.com>
2023-02-02 16:01:08 +0000

    variable declaration code added to js file

---
+++ assets/weather.js
@@ -0,0 +1,10 @@
+//This variable declared is to store the searched city
+var city="";
+// variable declared for each key parameter IDs
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var sCity=[];
