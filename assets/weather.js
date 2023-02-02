68d93b391d361660023dbc539a0e10cd6ef23a3b
oluwatayo Hassan <tayohas@gmail.com>
2023-02-02 16:03:30 +0000

    updated js file

--- assets/weather.js
+++ assets/weather.js
@@ -1,7 +1,7 @@
 //This variable declared is to store the searched city
 var city="";
 // variable declared for each key parameter IDs
-var searchCity = $("#search-city");
+var searchinput = $("#search-input");
 var searchButton = $("#search-button");
 var currentCity = $("#current-city");
 var currentTemperature = $("#temperature");
