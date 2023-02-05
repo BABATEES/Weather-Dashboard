5465b6541b6e7ae98705fabc8d02bfef30bc31ce
oluwatayo Hassan <tayohas@gmail.com>
2023-02-05 19:54:33 +0000

    declared variable for the API keys and created function to display current weather

--- assets/weather.js
+++ assets/weather.js
@@ -1,10 +1,32 @@
 //This variable declared is to store the searched city
-var city="";
+var city = "";
 // variable declared for each key parameter IDs
 var searchinput = $("#search-input");
 var searchButton = $("#search-button");
 var currentCity = $("#current-city");
 var currentTemperature = $("#temperature");
-var currentHumidty= $("#humidity");
-var currentWSpeed=$("#wind-speed");
-var sCity=[];
+var currentHumidty = $("#humidity");
+var currentWSpeed = $("#wind-speed");
+var sCity = [];
+
+// searches the city  in the entries from the storage
+function find(c) {
+    for (var i = 0; i < sCity.length; i++) {
+        if (c() === sCity[i]) {
+            return -1;
+        }
+    }
+    return 1;
+}
+//Declare variable for the API key
+var APIKey = "db51e3b23080eaf8ee12a93a875873cd";
+
+//function to display current weather
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchinput.val().trim() !== "") {
+        city = searchinput.val().trim();
+        currentWeather(city);
+    }
+}
+
